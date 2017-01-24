41030b1e02e9ebc79511c187fd208054dc429611
Jeffrey Shi <jeffrey.shi17@gmail.com>
2017-01-23 22:55:59 -0500

    update

--- script1.js
+++ script1.js
@@ -12,17 +12,15 @@ columnHints.push([2, 2]);
 columnHints.push([1, 1]);
 columnHints.push([2, 2]);
 columnHints.push([3]);
-tableCreate(w, h, rowHints, columnHints);
+var rHL = Math.max.apply(Math, rowHints.map(function (el) {
+    return el.length
+}));
+var cHL = Math.max.apply(Math, columnHints.map(function (el) {
+    return el.length
+}));
+tableCreate(w, h);
 
-function tableCreate(_w, _h, rH, cH) {
-    rHL = Math.max.apply(Math, rH.map(function (el) {
-        return el.length
-    }));
-    cHL = Math.max.apply(Math, cH.map(function (el) {
-        return el.length
-    }));
-    
-    
+function tableCreate(_w, _h) {
     var container = $("#container")
         , tbl = document.createElement('table');
     tbl.id = "tbl";
@@ -33,19 +31,18 @@ function tableCreate(_w, _h, rH, cH) {
         for (var j = 0; j < _w + rHL; j++) {
             var td = tr.insertCell();
             td.appendChild(document.createTextNode(''));
-            td.style.border = '1px solid black';
+            td.style.border = '1px solid gray';
             if (i < cHL && j < rHL) {
                 td.className = 'hintcross';
             }
             else if (i < cHL || j < rHL) {
                 td.className = 'hint';
                 if (i < cHL) {
-                    console.log(i,j);
-                    td.appendChild(document.createTextNode(columnHints[j-rHL][rHL-1-i]||""));
+                    console.log(i, j);
+                    td.appendChild(document.createTextNode(columnHints[j - rHL][rHL - 1 - i] || ""));
                 }
                 if (j < rHL) {
-                    td.appendChild(document.createTextNode(columnHints[i-cHL][cHL-1-j]||""));
-                    //td.appendChild(document.createTextNode(rowHints[i-cHL]));
+                    td.appendChild(document.createTextNode(columnHints[i - cHL][cHL - 1 - j] || ""));
                 }
             }
             else {
@@ -55,10 +52,20 @@ function tableCreate(_w, _h, rH, cH) {
     }
     container.append(tbl);
 }
-$('#tbl').find('td').click(function () {
-    console.log(this.parentNode.rowIndex + 1, this.cellIndex + 1);
+$('#tbl').find('td').on("mousedown mosueover", (function () {
+    //console.log(this.parentNode.rowIndex + 1, this.cellIndex + 1);
     $(this).toggleClass("on off");
     checkWin();
-});
+}));
 
-function checkWin() {}
+function checkWin() {
+    var table = document.getElementById("tbl");
+    for (var i = rHL, row; row = table.rows[i]; i++) {
+        for (var j = cHL, col; col = row.cells[j]; j++) {
+            $(col).toggleClass("on off");
+        }
+    }
+    for (var j = cHL, col; col = row.cells[j]; j++) {
+        for (var i = rHL, row; row = table.rows[i]; i++) {}
+    }
+}
